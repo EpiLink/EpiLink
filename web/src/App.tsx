7db31e7770d3ea70dfc988a3f8cfa2badf6a1cd9
Adrien Navratil <adrien1975@live.fr>
2020-03-23 16:11:54 +0100

    Basic footer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,8 +10,24 @@ export class App extends Component
     render()
     {
         return (
-            <div>
-                <h1>Hello world !</h1>
+            <div id="layout">
+                <div id="content">
+                    Salut
+                </div>
+
+                <div id="footer">
+                    <div id="left-footer">
+                        <img id="logo" src="../assets/logo.svg" />
+                        <span id="title">EpiLink</span>
+                        <span id="version">v1.0.0</span>
+                    </div>
+                    <ul id="navigation">
+                        <li class="navigation-item">Instance</li>
+                        <li class="navigation-item">Confidentialité</li>
+                        <li class="navigation-item">Sources</li>
+                        <li class="navigation-item">À Propos</li>
+                    </ul>
+                </div>
             </div>
         );
     }
