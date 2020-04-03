117c44e3f4aa6b31bb2438cedbdf5f65c8f9143d
Adrien Navratil <adrien1975@live.fr>
2020-04-03 14:58:22 +0200

    Update bot/src/org/epilink/bot/Main.kt
    Co-Authored-By: Zoroark <utybodev@gmail.com>

--- bot/src/org/epilink/bot/Main.kt
+++ bot/src/org/epilink/bot/Main.kt
@@ -68,6 +68,7 @@ fun main(args: Array<String>) = mainBody("epilink") {
             withContext(Dispatchers.IO) { // toRealPath blocks, resolve is also blocking
                 val path = cfgPath.parent.resolve(file)
                 logger.info("Loading rulebook from file $file (${path.toRealPath(LinkOption.NOFOLLOW_LINKS)}), this may take some time...")
+                // TODO When switching to Java 11, replace by Files.readString
                 val s = Files.readAllLines(path, StandardCharsets.UTF_8).joinToString("\n")
                 loadRules(s).also { rb -> logger.info("Rulebook loaded with ${rb.rules.size} rules.")}
             }
