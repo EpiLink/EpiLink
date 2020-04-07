06c09a3999a0952527fac65ab84ad7f776aa8ca7
utybo <utybodev@gmail.com>
2020-04-07 14:55:54 +0200

    Add test for Discord token retrival, fix scope missing in request

--- bot/test/org/epilink/bot/DiscordBackEndTest.kt
+++ bot/test/org/epilink/bot/DiscordBackEndTest.kt
@@ -2,8 +2,13 @@ package org.epilink.bot
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
-import io.mockk.mockk
-import org.epilink.bot.http.*
+import io.ktor.client.engine.mock.MockRequestHandler
+import io.ktor.client.engine.mock.respond
+import io.ktor.client.engine.mock.toByteArray
+import io.ktor.http.*
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.runBlocking
+import org.epilink.bot.http.LinkDiscordBackEnd
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
@@ -17,7 +22,7 @@ class DiscordBackEndTest : KoinTest {
     fun setupKoin() {
         startKoin {
             modules(module {
-                single { LinkDiscordBackEnd("Discord Client Id", "Discord Secret") }
+                single { LinkDiscordBackEnd("DiscordClientId", "DiscordSecret") }
             })
         }
     }
@@ -31,7 +36,7 @@ class DiscordBackEndTest : KoinTest {
     fun `Test Discord auth stub`() {
         val dbe = get<LinkDiscordBackEnd>()
         dbe.getAuthorizeStub().apply {
-            assertTrue(contains("client_id=Discord Client Id"), "Expected a client ID")
+            assertTrue(contains("client_id=DiscordClientId"), "Expected a client ID")
             assertTrue(contains("scope=identify"), "Expected the scope to be set to identify")
             assertTrue(contains("response_type=code"), "Expected the response type to be code")
             assertTrue(contains(Regex("scope=identify[&$]")), "Expected identify to be the only scope")
@@ -39,4 +44,49 @@ class DiscordBackEndTest : KoinTest {
             assertFalse(contains("redirect_uri"), "Expected redirect_uri to be absent")
         }
     }
+
+    @Test
+    fun `Test Discord token retrieval`() {
+        declareClientHandler(onlyMatchUrl = "https://discordapp.com/api/v6/oauth2/token") { request ->
+            assertEquals(HttpMethod.Post, request.method)
+            assertEquals(ContentType.Application.FormUrlEncoded, request.body.contentType)
+            @OptIn(KtorExperimentalAPI::class)
+            val params = String(request.body.toByteArray()).parseUrlEncodedParameters()
+            assertEquals("DiscordClientId", params["client_id"])
+            assertEquals("DiscordSecret", params["client_secret"])
+            assertEquals("authorization_code", params["grant_type"])
+            assertEquals("DiscordAuthCode", params["code"])
+            assertEquals("redir", params["redirect_uri"])
+            assertEquals("identify", params["scope"])
+            respond(
+                """{"access_token":"DiscordAccessToken"}""",
+                headers = headersOf("Content-Type", "application/json")
+            )
+        }
+
+        val dbe = get<LinkDiscordBackEnd>()
+        runBlocking {
+            assertEquals("DiscordAccessToken", dbe.getDiscordToken("DiscordAuthCode", "redir"))
+        }
+    }
+
+    private fun declareClientHandler(onlyMatchUrl: String? = null, handler: MockRequestHandler): HttpClient =
+        declare {
+            HttpClient(MockEngine) {
+                engine {
+                    addHandler(onlyMatchUrl?.let<String, MockRequestHandler> {
+                        { request ->
+                            when (request.url.fullUrl) {
+                                onlyMatchUrl -> handler(request)
+                                else -> error("Url ${request.url.fullUrl} does not match expected URL $onlyMatchUrl")
+                            }
+                        }
+                    } ?: handler)
+                }
+            }
+        }
 }
+
+// From https://ktor.io/clients/http-client/testing.html
+private val Url.hostWithPortIfRequired: String get() = if (port == protocol.defaultPort) host else hostWithPort
+private val Url.fullUrl: String get() = "${protocol.name}://$hostWithPortIfRequired$fullPath"
