5c4089f996d65aa84a08951a61c14558b9c07cfb
utybo <utybodev@gmail.com>
2020-04-09 23:32:16 +0200

    Add tests for microsoft check and general creation

--- bot/src/test/kotlin/org/epilink/bot/DatabaseTest.kt
+++ bot/src/test/kotlin/org/epilink/bot/DatabaseTest.kt
@@ -10,6 +10,7 @@ import org.koin.test.KoinTest
 import org.koin.test.get
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
+import java.time.LocalDateTime
 import kotlin.test.*
 
 class DatabaseTest : KoinTest {
@@ -17,7 +18,7 @@ class DatabaseTest : KoinTest {
     fun setupKoin() {
         startKoin {
             modules(module {
-                single<LinkServerDatabase> { LinkServerDatabaseImpl() }
+                single<LinkServerDatabase> { spyk(LinkServerDatabaseImpl()) }
             })
         }
     }
@@ -28,7 +29,7 @@ class DatabaseTest : KoinTest {
     }
 
     @Test
-    fun `Test Discord user who already exists cannot create account (advisory)`() {
+    fun `Test Discord user who already exists cannot create account`() {
         mockHere<LinkDatabaseFacade> {
             coEvery { doesUserExist("discordid") } returns true
         }
@@ -43,24 +44,7 @@ class DatabaseTest : KoinTest {
     }
 
     @Test
-    fun `Test Discord user who already exists cannot create account (creation failure)`() {
-        mockHere<LinkDatabaseFacade> {
-            coEvery { doesUserExist("discordid") } returns true
-            coEvery { getBansFor(any()) } returns listOf()
-            coEvery { isMicrosoftAccountAlreadyLinked(any()) } returns false
-        }
-        test {
-            val exc = assertFailsWith<LinkEndpointException> {
-                createUser("discordid", "", "", true)
-            }
-            assertEquals(StandardErrorCodes.AccountCreationNotAllowed, exc.errorCode)
-            assertTrue(exc.isEndUserAtFault, "End user should be considered to be at fault")
-            assertTrue(exc.message!!.contains("already exists"))
-        }
-    }
-
-    @Test
-    fun `Test Discord user who does not exist should be able to create account (advisory)`() {
+    fun `Test Discord user who does not exist should be able to create account`() {
         mockHere<LinkDatabaseFacade> {
             coEvery { doesUserExist("discordid") } returns false
         }
@@ -71,19 +55,111 @@ class DatabaseTest : KoinTest {
     }
 
     @Test
+    fun `Test Microsoft user who already exists cannot create account`() {
+        val hey = "hey".sha256()
+        mockHere<LinkDatabaseFacade> {
+            coEvery { isMicrosoftAccountAlreadyLinked(hey) } returns true
+        }
+        test {
+            val adv = isMicrosoftUserAllowedToCreateAccount("hey")
+            assertTrue(adv is Disallowed, "Creation should be disallowed")
+            assertTrue(adv.reason.contains("already"), "Reason should contain word already")
+        }
+    }
+
+    @Test
+    fun `Test indefinitely banned Microsoft user cannot create account`() {
+        val hey = "hey".sha256()
+        mockHere<LinkDatabaseFacade> {
+            coEvery { isMicrosoftAccountAlreadyLinked(hey) } returns false
+            coEvery { getBansFor(hey) } returns listOf(mockk {
+                every { expiresOn } returns null
+            })
+        }
+        test {
+            val adv = isMicrosoftUserAllowedToCreateAccount("hey")
+            assertTrue(adv is Disallowed, "Creation should be disallowed")
+            assertTrue(adv.reason.contains("banned"), "Reason should contain word banned")
+        }
+    }
+
+    @Test
+    fun `Test temporarily banned Microsoft user cannot create account`() {
+        val hey = "hey".sha256()
+        mockHere<LinkDatabaseFacade> {
+            coEvery { isMicrosoftAccountAlreadyLinked(hey) } returns false
+            coEvery { getBansFor(hey) } returns listOf(mockk {
+                every { expiresOn } returns LocalDateTime.now().plusDays(1)
+            })
+        }
+        test {
+            val adv = isMicrosoftUserAllowedToCreateAccount("hey")
+            assertTrue(adv is Disallowed, "Creation should be disallowed")
+            assertTrue(adv.reason.contains("banned"), "Reason should contain word banned")
+        }
+    }
+
+    @Test
+    fun `Test temporarily banned Microsoft user with ban expired can create account`() {
+        val hey = "hey".sha256()
+        mockHere<LinkDatabaseFacade> {
+            coEvery { isMicrosoftAccountAlreadyLinked(hey) } returns false
+            coEvery { getBansFor(hey) } returns listOf(mockk {
+                every { expiresOn } returns LocalDateTime.now().minusSeconds(1)
+            })
+        }
+        test {
+            val adv = isMicrosoftUserAllowedToCreateAccount("hey")
+            assertTrue(adv is Allowed)
+        }
+    }
+
+    @Test
     fun `Successful account creation`() {
         val hash = "tested".sha256()
         val fac = mockHere<LinkDatabaseFacade> {
-            coEvery { doesUserExist("discordid") } returns false
-            coEvery { getBansFor(hash) } returns listOf()
-            coEvery { isMicrosoftAccountAlreadyLinked(hash) } returns false
             coEvery { recordNewUser("discordid", hash, "eeemail", true, any()) } returns mockk()
         }
         test {
+            coEvery { isMicrosoftUserAllowedToCreateAccount("tested") } returns Allowed
+            coEvery { isDiscordUserAllowedToCreateAccount("discordid") } returns Allowed
             createUser("discordid", "tested", "eeemail", true)
+            coVerify { isMicrosoftUserAllowedToCreateAccount("tested") }
+            coVerify { isDiscordUserAllowedToCreateAccount("discordid") }
         }
         coVerify { fac.recordNewUser("discordid", hash, "eeemail", true, any()) }
     }
+
+    @Test
+    fun `Unsuccessful account creation on msft issue`() {
+        test {
+            coEvery { isMicrosoftUserAllowedToCreateAccount("tested") } returns Disallowed("Hello")
+            coEvery { isDiscordUserAllowedToCreateAccount("discordid") } returns Allowed
+            val exc = assertFailsWith<LinkEndpointException> {
+                createUser("discordid", "tested", "eeemail", true)
+            }
+            coVerify { isMicrosoftUserAllowedToCreateAccount("tested") }
+            assertEquals(StandardErrorCodes.AccountCreationNotAllowed, exc.errorCode)
+            assertTrue(exc.isEndUserAtFault, "End user is expected to be at fault")
+            assertTrue(exc.message!!.contains("Hello"))
+        }
+    }
+
+    @Test
+    fun `Unsuccessful account creation on Discord issue`() {
+        test {
+            coEvery { isMicrosoftUserAllowedToCreateAccount("tested") } returns Allowed
+            coEvery { isDiscordUserAllowedToCreateAccount("discordid") } returns Disallowed("Hiii")
+            val exc = assertFailsWith<LinkEndpointException> {
+                createUser("discordid", "tested", "eeemail", true)
+            }
+            coVerify { isDiscordUserAllowedToCreateAccount("discordid") }
+            assertEquals(StandardErrorCodes.AccountCreationNotAllowed, exc.errorCode)
+            assertTrue(exc.isEndUserAtFault, "End use is expected to be at fault")
+            assertTrue(exc.message!!.contains("Hiii"))
+        }
+    }
+
 
     private fun <R> test(block: suspend LinkServerDatabase.() -> R) =
         runBlocking {
