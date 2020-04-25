83ae12ae9bf25e70d7ecf81122006a56642f73e9
utybo <utybodev@gmail.com>
2020-04-25 15:55:05 +0200

    Update license headers

--- web/src/components/Loading.vue
+++ web/src/components/Loading.vue
@@ -1,3 +1,13 @@
+<!--
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    This Source Code Form is "Incompatible With Secondary Licenses", as
+    defined by the Mozilla Public License, v. 2.0.
+
+-->
 <template>
     <div class="loading">
         <div v-for="_ of new Array(13)" class="loading-tick">
