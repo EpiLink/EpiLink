1b003c6d58c1c803180de50b160acaf27a360664
utybo <utybodev@gmail.com>
2020-07-15 00:05:02 +0200

    Add file test for Discord I18n class

---
+++ bot/src/test/kotlin/org/epilink/bot/DiscordMessagesI18nTest.kt
@@ -0,0 +1,103 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+package org.epilink.bot
+
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import org.epilink.bot.db.LinkDatabaseFacade
+import org.epilink.bot.discord.LinkDiscordMessagesI18n
+import org.epilink.bot.discord.LinkDiscordMessagesI18nImpl
+import org.koin.dsl.module
+import org.koin.test.get
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class DiscordMessagesI18nTest : KoinBaseTest(
+    module {
+        single<LinkDiscordMessagesI18n> {
+            LinkDiscordMessagesI18nImpl(
+                mapOf(
+                    "one" to mapOf("hello" to "Bonjour", "goodbye" to "Au revoir"),
+                    "two" to mapOf("hello" to "Buongiorno")
+                ),
+                "one"
+            )
+        }
+    }
+) {
+
+    @Test
+    fun `Test available languages`() {
+        test {
+            assertEquals(setOf("one", "two"), availableLanguages)
+        }
+    }
+    @Test
+    fun `Test key retrieval present in chosen`() {
+        test {
+            assertEquals("Buongiorno", get("two", "hello"))
+        }
+    }
+
+    @Test
+    fun `Test key fallback`() {
+        test {
+            assertEquals("Au revoir", get("two", "goodbye"))
+        }
+    }
+
+    @Test
+    fun `Test set language with valid language`() {
+        val db = mockHere<LinkDatabaseFacade> {
+            coEvery { recordLanguagePreference("did", "two") } just runs
+        }
+        test { assertTrue(setLanguage("did", "two")) }
+        coVerify { db.recordLanguagePreference("did", "two") }
+    }
+
+    @Test
+    fun `Test set language with invalid language`() {
+        test { assertFalse(setLanguage("did", "three")) }
+    }
+
+    @Test
+    fun `Test get language no id`() {
+        test { assertEquals("one", getLanguage(null)) }
+    }
+
+    @Test
+    fun `Test get language no preference`() {
+        mockHere<LinkDatabaseFacade> {
+            coEvery { getLanguagePreference("did") } returns null
+        }
+        test { assertEquals("one", getLanguage("did")) }
+    }
+
+    @Test
+    fun `Test get language invalid preference`() {
+        mockHere<LinkDatabaseFacade> {
+            coEvery { getLanguagePreference("did") } returns "LQSDLKJQHSD"
+        }
+        test { assertEquals("one", getLanguage("did")) }
+    }
+
+    @Test
+    fun `Test get language valid preference`() {
+        mockHere<LinkDatabaseFacade> {
+            coEvery { getLanguagePreference("did") } returns "two"
+        }
+        test { assertEquals("two", getLanguage("did")) }
+    }
+
+    private fun test(block: suspend LinkDiscordMessagesI18n.() -> Unit) {
+        runBlocking { get<LinkDiscordMessagesI18n>().block() }
+    }
+}
